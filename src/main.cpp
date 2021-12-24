06889f49564f55a0e5ae76e810fb464e05b221d7
jonas <jonasmuehlmann@protonmail.com>
2021-12-24 16:19:47 +0100

    chore: ignore files and fix CMake config

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
+#include <fstream>
+#include <streambuf>
+#include <string>
+
 int main(int argc, char* argv[])
 {
+    if (argc == 1)
+    {
+        return 1;
+    }
+    std::ifstream source_stream(argv[1]);
+    std::string   source_code((std::istreambuf_iterator<char>(source_stream)),
+                            std::istreambuf_iterator<char>());
     return 0;
 }
---
+++ src/parser.hpp
@@ -0,0 +1,4 @@
+#ifndef SRC_PARSER_HPP
+#define SRC_PARSER_HPP
+
+#endif    // SRC_PARSER_HPP
