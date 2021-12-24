68d15897b1e4ea9f31d1217826222a3ee2e2d425
jonas <jonasmuehlmann@protonmail.com>
2021-12-24 15:55:07 +0100

    feat: Define tokens

---
+++ src/tokens.hpp
@@ -0,0 +1,41 @@
+enum class Tokens
+{
+    // Syntax
+    LPAREN,
+    RPAREN,
+    LBRACKET,
+    RBRACKET,
+    LBRACAE,
+    RBRACE,
+    SEMICOLON,
+    COMMA,
+    // Operators
+    LESS,
+    GREATER,
+    EQUAL,
+    NOT,
+    LOGICAL_AND,
+    LOGICAL_OR,
+    BINARY_AND,
+    BINARY_OR,
+    LSHIFT,
+    RSHIFT,
+    XOR,
+    PLUS,
+    MINUS,
+    MULTIPLICATION,
+    DIVISION,
+    MODULO,
+    INCREMENT,
+    DECREMENT,
+    // Flow control
+    WHILE,
+    FOR,
+    IF,
+    ELIF,
+    ELSE,
+    // Misc
+    FUNCTION,
+    PROCEDURE,
+    RETURN
+};
