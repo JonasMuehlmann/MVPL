b65e1cecb1c7ce07a25787f0f1f9a2f58ba1775b
jonas <jonasmuehlmann@protonmail.com>
2022-01-07 19:47:11 +0100

    chore: add CMake configs for test and benchmark code

---
+++ benchmarks/parser_benchmarks.cpp
@@ -0,0 +1,15 @@
+#include <benchmark/benchmark.h>
+
+static void BM_SomeFunction(benchmark::State& state)
+{
+    // Perform setup here
+    for (auto _ : state)
+    {
+        // This code gets timed
+        (void)0;
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_SomeFunction);
+// Run the benchmark
+BENCHMARK_MAIN();
