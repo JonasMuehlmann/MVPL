17282ea17478c438779c71fed701d30c6a664f74
jonas <jonasmuehlmann@protonmail.com>
2022-01-08 17:05:39 +0100

    test: add basic tests for parsing parameter definitions

--- tests/frontend/parser/parser_tests.cpp
+++ tests/frontend/parser/parser_tests.cpp
@@ -1,9 +1,137 @@
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions)
+#include <algorithm>
+#include <array>
+#include <span>
+#include <variant>
+#include <vector>
+
+#include "../src/frontend/lexer/token.hpp"
+#include "../src/frontend/lexer/token_type.hpp"
+#include "../src/frontend/parser/parser.hpp"
+
+using namespace std::string_view_literals;
+
+// FIX: Passing empty token streams to a parser causes a segfault
+// TEST(TestParameterDefParser, Empty)
+// {
+//     std::array<token, 0> token_stream_raw{};
+
+//     std::span<token> token_stream(token_stream_raw);
+
+//     auto result = parameter_def_parser::parse(token_stream);
+// }
+
+TEST(TestParameterDefParser, OneParameter)
 {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+    std::array token_stream_raw{
+        token(token_type::LPAREN, "("sv, source_location()),
+        token(token_type::IDENTIFIER, "foo"sv, source_location()),
+        token(token_type::RPAREN, ")"sv, source_location())};
+
+    std::span<token> token_stream(token_stream_raw);
+
+    auto result = parameter_def_parser::parse(token_stream);
+
+    ASSERT_TRUE(result.has_value());
+    ASSERT_EQ(get_token_stream(result).size(), 0);
+    ASSERT_NE(get_node(result), nullptr);
+    ASSERT_TRUE(std::holds_alternative<parameter_def_node>((*get_node(result))));
+    ASSERT_EQ(std::get<parameter_def_node>((*get_node(result))).parameter_list.size(),
+              1);
+    ASSERT_EQ(std::get<parameter_def_node>((*get_node(result))).parameter_list[0],
+              "foo"sv);
+}
+
+TEST(TestParameterDefParser, TwoParameters)
+{
+    std::array token_stream_raw{
+        token(token_type::LPAREN, "("sv, source_location()),
+        token(token_type::IDENTIFIER, "foo"sv, source_location()),
+        token(token_type::COMMA, ","sv, source_location()),
+        token(token_type::IDENTIFIER, "bar"sv, source_location()),
+        token(token_type::RPAREN, ")"sv, source_location())};
+
+    std::span<token> token_stream(token_stream_raw);
+
+    auto result = parameter_def_parser::parse(token_stream);
+
+    ASSERT_TRUE(result.has_value());
+    ASSERT_EQ(get_token_stream(result).size(), 0);
+    ASSERT_NE(get_node(result), nullptr);
+    ASSERT_TRUE(std::holds_alternative<parameter_def_node>((*get_node(result))));
+    ASSERT_EQ(std::get<parameter_def_node>((*get_node(result))).parameter_list.size(),
+              2);
+    ASSERT_TRUE(std::ranges::equal(
+        std::get<parameter_def_node>((*get_node(result))).parameter_list,
+        std::array{"foo"sv, "bar"sv}));
+}
+
+TEST(TestParameterDefParser, ThreeParameters)
+{
+    std::array token_stream_raw{
+        token(token_type::LPAREN, "("sv, source_location()),
+        token(token_type::IDENTIFIER, "foo"sv, source_location()),
+        token(token_type::COMMA, ","sv, source_location()),
+        token(token_type::IDENTIFIER, "bar"sv, source_location()),
+        token(token_type::COMMA, ","sv, source_location()),
+        token(token_type::IDENTIFIER, "baz"sv, source_location()),
+        token(token_type::RPAREN, ")"sv, source_location())};
+
+    std::span<token> token_stream(token_stream_raw);
+
+    auto result = parameter_def_parser::parse(token_stream);
+
+    ASSERT_TRUE(result.has_value());
+    ASSERT_EQ(get_token_stream(result).size(), 0);
+    ASSERT_NE(get_node(result), nullptr);
+    ASSERT_TRUE(std::holds_alternative<parameter_def_node>((*get_node(result))));
+    ASSERT_EQ(std::get<parameter_def_node>((*get_node(result))).parameter_list.size(),
+              3);
+    ASSERT_TRUE(std::ranges::equal(
+        std::get<parameter_def_node>((*get_node(result))).parameter_list,
+        std::array{"foo"sv, "bar"sv, "baz"sv}));
+}
+
+TEST(TestParameterDefParser, FourParameters)
+{
+    std::array token_stream_raw{
+        token(token_type::LPAREN, "("sv, source_location()),
+        token(token_type::IDENTIFIER, "foo"sv, source_location()),
+        token(token_type::COMMA, ","sv, source_location()),
+        token(token_type::IDENTIFIER, "bar"sv, source_location()),
+        token(token_type::COMMA, ","sv, source_location()),
+        token(token_type::IDENTIFIER, "baz"sv, source_location()),
+        token(token_type::COMMA, ","sv, source_location()),
+        token(token_type::IDENTIFIER, "test"sv, source_location()),
+        token(token_type::RPAREN, ")"sv, source_location())};
+
+    std::span<token> token_stream(token_stream_raw);
+
+    auto result = parameter_def_parser::parse(token_stream);
+
+    ASSERT_TRUE(result.has_value());
+    ASSERT_EQ(get_token_stream(result).size(), 0);
+    ASSERT_NE(get_node(result), nullptr);
+    ASSERT_TRUE(std::holds_alternative<parameter_def_node>((*get_node(result))));
+    ASSERT_EQ(std::get<parameter_def_node>((*get_node(result))).parameter_list.size(),
+              4);
+    ASSERT_TRUE(std::ranges::equal(
+        std::get<parameter_def_node>((*get_node(result))).parameter_list,
+        std::array{"foo"sv, "bar"sv, "baz"sv, "test"sv}));
+}
+
+TEST(TestParameterDefParser, TrailingComma)
+{
+    std::array token_stream_raw{
+        token(token_type::LPAREN, "("sv, source_location()),
+        token(token_type::IDENTIFIER, "foo"sv, source_location()),
+        token(token_type::COMMA, ","sv, source_location()),
+        token(token_type::RPAREN, ")"sv, source_location())};
+
+    std::span<token> token_stream(token_stream_raw);
+
+    auto result = parameter_def_parser::parse(token_stream);
+
+    ASSERT_FALSE(result.has_value());
 }
