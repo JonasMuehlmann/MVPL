ebebebfeb2625c0f2a9577e37b7c0942e524c6b1
jonas <jonasmuehlmann@protonmail.com>
2022-01-17 12:03:51 +0100

    chore: split frontend/parser/util into header annd implementation

--- src/frontend/parser/util.hpp
+++ src/frontend/parser/util.hpp
@@ -53,122 +53,27 @@ using parse_result  = std::variant<parse_content, parse_error>;
 //****************************************************************************//
 //                              Helper functions                              //
 //****************************************************************************//
-std::span<token>& get_token_stream(parse_result& result)
-{
-    return std::get<0>(std::get<parse_content>(result));
-}
+std::span<token>& get_token_stream(parse_result& result);
 
-std::unique_ptr<ast_node_t>& get_node(parse_result& result)
-{
-    return std::get<1>(std::get<parse_content>(result));
-}
+std::unique_ptr<ast_node_t>& get_node(parse_result& result);
 
-source_location get_source_location_from_compound(std::vector<parse_result>& nodes)
-{
-    auto first_node = get_node(nodes.front()).get();
-    auto last_node  = get_node(nodes.back()).get();
-
-    auto start_location = std::visit(source_location_retriever_visitor{}, *first_node);
-    auto end_location   = std::visit(source_location_retriever_visitor{}, *last_node);
-
-    return {start_location.line_start,
-            start_location.col_start,
-            end_location.line_end,
-            end_location.col_end};
-}
+source_location get_source_location_from_compound(std::vector<parse_result>& nodes);
 
 bool try_add_parse_result(parse_result&&             cur_result,
                           std::vector<parse_result>& results,
                           std::span<token>&          ts,
-                          bool                       overwrite_errors = false)
-{
-    if (std::holds_alternative<parse_error>(cur_result))
-    {
-        if (std::ranges::none_of(results, [](auto& result) {
-                return std::holds_alternative<parse_error>(result);
-            }))
-        {
-            results.push_back(std::move(cur_result));
-        }
-
-        return false;
-    }
-
-
-    if (overwrite_errors)
-    {
-        std::erase_if(results, [](auto& cur_result) {
-            return std::holds_alternative<parse_error>(cur_result);
-        });
-    }
-
-    results.push_back(std::move(cur_result));
-
-    ts = get_token_stream(results.back());
-
-    return true;
-}
+                          bool                       overwrite_errors = false);
 
 bool try_add_parse_result(parse_result&&    cur_result,
                           parse_result&     result,
                           std::span<token>& ts,
-                          bool              overwrite_errors = false)
-{
-    result = std::move(cur_result);
-
-    if (std::holds_alternative<parse_content>(result))
-    {
-        ts = get_token_stream(result);
-
-        return true;
-    }
-    return false;
-}
+                          bool              overwrite_errors = false);
 
 bool try_add_parse_result(std::vector<parse_result>&& cur_results,
                           std::vector<parse_result>&  results,
                           std::span<token>&           ts,
-                          bool                        overwrite_errors = false)
-{
-    if (std::ranges::all_of(cur_results, [](auto& cur_result) {
-            return std::holds_alternative<parse_content>(cur_result);
-        }))
-    {
-        std::ranges::move(cur_results, std::back_inserter(results));
+                          bool                        overwrite_errors = false);
 
-        if (overwrite_errors)
-        {
-            std::erase_if(results, [](auto& cur_result) {
-                return std::holds_alternative<parse_error>(cur_result);
-            });
-        }
-        ts = get_token_stream(results.back());
+bool is_any_parse_result_valid(std::vector<parse_result>& results);
 
-        return true;
-    }
-
-    if (std::ranges::none_of(results, [](auto& cur_result) {
-            return std::holds_alternative<parse_error>(cur_result);
-        }))
-    {
-        results.push_back(
-            std::move(*std::ranges::find_if(cur_results, [](auto& cur_result) {
-                return std::holds_alternative<parse_error>(cur_result);
-            })));
-    }
-
-    return false;
-}
-bool is_any_parse_result_valid(std::vector<parse_result>& results)
-{
-    return std::ranges::any_of(results, [](auto& parse_result_) {
-        return std::holds_alternative<parse_content>(parse_result_);
-    });
-}
-
-bool are_all_parse_results_valid(std::vector<parse_result>& results)
-{
-    return std::ranges::all_of(results, [](auto& parse_result_) {
-        return std::holds_alternative<parse_content>(parse_result_);
-    });
-}
+bool are_all_parse_results_valid(std::vector<parse_result>& results);
