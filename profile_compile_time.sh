a4584a8428208094d3b25a0ae666800ad9bb8854
jonas <jonasmuehlmann@protonmail.com>
2022-02-01 14:39:47 +0100

    chore(tooling): improve script for profiling compile times

--- profile_compile_time.sh
+++ profile_compile_time.sh
@@ -5,6 +5,12 @@ output_file="clang_build_analyzer.txt"
 
 touch "${output_file}"
 
+rm cmake-build-debug/CMakeCache.txt
+
+export MVPL_PROFILE_COMPILE_TIME=1
+./build_main.sh
+
 fd cpp.json cmake-build-debug/src | xargs cp -t "${trace_dir}"
+
 ClangBuildAnalyzer --all "${trace_dir}" "${output_file}"
 ClangBuildAnalyzer --analyze "${output_file}"
