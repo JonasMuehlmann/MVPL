687fe969a580f9a67c95b85087db6157e0d49d14
jonas <jonasmuehlmann@protonmail.com>
2022-02-11 12:45:14 +0100

    feat(sem_analysis): prepare files needed for semantic analysis

--- src/frontend/semantic_analysis/symbol_table.cpp
+++ src/frontend/semantic_analysis/symbol_table.cpp
@@ -7,5 +7,7 @@ symbol_table build_symbol_table(ast_node_t ast)
     scope_node   scope_tree("global", nullptr);
     symbol_table symbol_table;
 
+    // TODO: Add visitor functions for listing ast_node_t
+
     return symbol_table;
 }
